f69f97d3c87664cea2d9895beb05375946f86fd8
Tristan <tristan.doehl@holy-mail.de>
2020-01-29 13:44:36 +0100

    Chore: release.js

--- release.js
+++ release.js
@@ -3,21 +3,27 @@ const exec = util.promisify(require('child_process').exec);
 
 const level = process.argv[2];
 
+const print = ({stdout, stderr}) => {
+  console.log(stdout);
+  console.log(stderr);
+} ;
+
+
 if (level === undefined) {
   console.log('Provide a patch-level: [major, minor, patch]');
   process.exit(1);
 }
 
 const run = async () => {
-  await exec('npm run build').then(console.log);
+  await exec('npm run build').then(print);
   try {
-    await exec('git commit -am "Chore: updating version ' + level + '"').then(console.log);
+    await exec('git commit -am "Chore: updating version ' + level + '"').then(print);
   } catch {
 
   }
-  await exec('npm version ' + level).then(console.log);
-  await exec('git add --all').then(console.log);
-  await exec('git push --all && git push --tags').then(console.log);
+  await exec('npm version ' + level).then(print);
+  await exec('git add --all').then(print);
+  await exec('git push --all && git push --tags').then(print);
 
 };
 
