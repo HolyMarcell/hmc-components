4d0f56cdfbd55c4331c9a4012520a785bf9f63d3
Tristan <tristan.doehl@holy-mail.de>
2020-03-04 11:44:00 +0100

    Feat: Remove unneccesary re-render in loadingUntil

--- src/datamangagement/LoadingUntil.tsx
+++ src/datamangagement/LoadingUntil.tsx
@@ -1,31 +1,26 @@
 import React from "react";
-import {isNil} from 'ramda';
 import {LoadingUntilProps} from "./types";
 import Icon from "../icon/Icon";
 
 
 const LoadingUntil: React.FC<LoadingUntilProps> =
-  ({loading, hasData = false, alt, children, className = '', style = {}}) => {
+  ({loading = false, hasData = false,  children, className = '', style = {}}) => {
 
-    if (!loading && !isNil(loading)) {
-      return <>{children || null}</>; // some weird typescript fuckup?
-    }
 
-    if (loading && hasData) {
-      return <div className={'loading-overlay'}>
+    const showChildren = loading ? hasData : true;
+
+    return (
+      <div className={`${loading && hasData ? 'loading-overlay' : ''}`}>
+        {loading && hasData &&
         <div className={className} style={style}>
           <Icon icon={'spinner'} className={'fa-spin fa-3x spinner'}/>
         </div>
-        {children}
-      </div>
-    }
+        }
+        {showChildren &&
 
-    return !isNil(alt) ?
-      <>{alt}</>
-      :
-      <div className={className} style={style}>
-        <Icon icon={'spinner'} className={'fa-spin fa-3x'}/>
+        children} 
       </div>
+    );
   };
 
 
